f714701b8bee26d0c5cfd8a221ae50fd13173d06
xyzsince2014 <xyzsince2014@gmail.com>
2026-02-12 23:59:12 +0900

    changed ip, etc

--- docker-run.sh
+++ docker-run.sh
@@ -4,5 +4,5 @@ docker container run -d --rm \
   -v $(pwd)/app/tomcat/logs:/usr/share/tomcat/logs \
   --name tokyomap-oauth \
   --net network_tokyomap \
-  --ip 192.168.56.110 \
+  --ip 172.20.0.110 \
   tokyomap.oauth:dev
